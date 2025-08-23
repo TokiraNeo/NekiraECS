8134d47e17d996e1209088f742e4dca605247794
TokiraNeo <TokiraNeo@outlook.com>
2025-08-23 23:04:55 +0800

    引入Component，Component数组接口

--- include/NekiraECS/Core/Entity/Entity.hpp
+++ include/NekiraECS/Core/Entity/Entity.hpp
@@ -8,100 +8,101 @@
 
 #pragma once
 
-
+#include <NekiraECS/Core/Template/TSingleton.hpp>
 #include <cstdint>
 #include <stack>
 #include <vector>
+
+
+namespace NekiraECS
+{
+
+// 实体ID的类型定义. [Index(16 bits) | Version(16 bits)]
+using EntityID_t = uint32_t;
+
+// 定义一个无效的实体ID常量
+constexpr EntityID_t INVALID_ENTITYID = 0;
+
+// 实体索引的类型定义
+using EntityIndex_t = uint16_t;
+
+// 实体版本的类型定义
+using EntityVersion_t = uint16_t;
+
+// 定义实体ID掩码
+constexpr EntityIndex_t ENTITY_INDEX_MASK = 0xFFFF;
+
+// 定义右移位数
+constexpr uint8_t ENTITY_INDEX_SHIFT = 16;
+
+} // namespace NekiraECS
 
 
 
 namespace NekiraECS
 {
 
+// 实体类
 struct Entity final
 {
-    // 友元类EntityManager，只能由EntityManager创建和管理Entity实例
     friend class EntityManager;
 
 public:
-    // 禁止拷贝和移动构造函数
-    Entity(const Entity&) = delete;
-    Entity(Entity&&) = delete;
-
-    // 禁止拷贝和移动赋值操作符
-    Entity& operator=(const Entity&) = delete;
-    Entity& operator=(Entity&&) = delete;
-
-    // 比较操作符
-    bool operator==(const Entity& other) const
-    {
-        return ID == other.ID;
-    }
-
-    bool operator!=(const Entity& other) const
-    {
-        return !(*this == other);
-    }
-
-private:
-    // 私有化构造函数，确保Entity无法被外部直接实例化，只能通过EntityManager创建
-    // 这也确保了每个Entity的ID是唯一且受控的
-
-    Entity() = default;
-
-    explicit Entity(uint32_t id) : ID(id)
+    // 外部直接构建的为无效实体
+    Entity() : ID(INVALID_ENTITYID)
     {}
 
 private:
-    // Entity的唯一标识符,由两部分组成：
-    // - 高16位：表示Entity的索引
-    // - 低16位：表示Entity的版本
-    // ID: [Index(16 bits)][Version(16 bits)]
-    uint32_t ID;
-};
-
-
-class EntityManager final
-{
-public:
-    // 禁止拷贝和移动构造函数
-    EntityManager(const EntityManager&) = delete;
-    EntityManager(EntityManager&&) = delete;
-
-    // 禁止拷贝和移动赋值操作符
-    EntityManager& operator=(const EntityManager&) = delete;
-    EntityManager& operator=(EntityManager&&) = delete;
-
-    // 获取单例实例
-    static EntityManager& Get()
-    {
-        static EntityManager instance;
-        return instance;
-    }
-
-    // 判断Entity是否有效
-    bool CheckEntity(const Entity& entity) const;
-    bool CheckEntity(uint16_t index, uint16_t version) const;
-
-    // 创建一个新的Entity实例
-    Entity CreateEntity();
-
-    // 销毁一个Entity实例
-    void DestroyEntity(const Entity& entity);
-
-    // 获取Entity的索引与版本号
-    static void DecodeEntity(const Entity& entity, uint16_t& OutIndex, uint16_t& OutVersion);
-    static void DecodeEntity(uint32_t ID, uint16_t& OutIndex, uint16_t& OutVersion);
+    // 私有构造函数，仅允许EntityManager创建实体
+    explicit Entity(EntityID_t id) : ID(id)
+    {}
 
 private:
-    EntityManager() = default;
+    // 实体的唯一标识符
+    // 由索引和版本组成 (高16位为索引，低16位为版本)
+    // [Index(16 bits) | Version(16 bits)]
+    EntityID_t ID;
+};
 
-    // 存储每个Entity索引对应的版本号
-    std::vector<uint16_t> Versions;
+} // namespace NekiraECS
 
-    // 存储可重用的Entity ID(Index+Version),
-    // 回收池里的ID在回收时已经做了版本号递增处理，因此可以直接使用
-    std::stack<uint32_t> RecycledIDs;
+
+
+namespace NekiraECS
+{
+
+// 实体管理器单例
+class EntityManager final : public TSingleton<EntityManager>
+{
+public:
+    // 解析实体
+    static void DecodeEntity(const Entity& entity, EntityIndex_t& outIndex, EntityVersion_t& outVersion);
+
+    // 获取实体的索引
+    static EntityIndex_t GetEntityIndex(const Entity& entity);
+    static EntityIndex_t GetEntityIndex(EntityID_t entityID);
+
+    // 获取实体的版本
+    static EntityVersion_t GetEntityVersion(const Entity& entity);
+    static EntityVersion_t GetEntityVersion(EntityID_t entityID);
+
+    // 实体是否有效
+    bool IsValid(const Entity& entity) const;
+    bool IsValid(EntityID_t entityID) const;
+
+    // 创建一个新实体
+    Entity CreateEntity();
+
+    // 销毁一个实体
+    void DestroyEntity(const Entity& entity);
+    void DestroyEntity(EntityID_t entityID);
+
+private:
+    // 每个实体的版本号 EntityIndex -> EntityVersion
+    std::vector<EntityVersion_t> EntityVersions;
+
+    // 可复用的实体ID.版本号在回收时已做+1处理，因此可以直接复用
+    std::stack<EntityID_t> RecycledIDs;
 };
 
 } // namespace NekiraECS
